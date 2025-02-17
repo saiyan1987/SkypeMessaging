a444051149ed819ae91327a0f8202d88b83f0b5c
saiyan1987 <faheem@sky.com>
2025-02-17 11:15:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import skpy
 import argparse
 
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--username",dest="username")
     parser.add_argument("--password", dest="password")
     parser.add_argument("--message",dest="message")
     args=parser.parse_args()
-    skype=skpy.Skype(args.username,args.password)
-
-    white_list=["zicky.khanzada","cadbury_omi"]
+    skype=skpy.Skype()
+    skype.conn.liveLogin(args.username,args.password)
+    white_list=["uk-faheem"]
     for contact in skype.contacts:
         if(contact.id in white_list):
             print(f"sending message to {contact.id}")
